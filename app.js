c7834de81607b77e4bb9bd7cced039b16c66381f
arijeet <arijeetsinha2@gmail.com>
2023-10-25 16:51:07 +0530

    changed

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fav = require('./favorites');
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://wiindy.vercel.app', // Replace with the actual URL of your React app
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
